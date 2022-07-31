c084477654318a2dc4dfe901975328be0e450cf4
IsHaltEchtSo <d.grollmusz@gmail.com>
2022-07-31 15:02:44 +0200

    schema to (de)serialize user data

---
+++ rest_api/schemas.py
@@ -0,0 +1,12 @@
+from models import User
+from marshmallow import Schema, fields, post_load
+
+class UserSchema(Schema):
+    name = fields.String()
+    age = fields.Integer()
+    follower = fields.List(fields.Nested('UserSchema'), default=[])
+    following = fields.List(fields.Nested('UserSchema'), default=[])
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
