446245fd25c6de2617fb99d1c2998f9b1fd38112
IsHaltEchtSo <d.grollmusz@gmail.com>
2022-07-31 21:53:06 +0200

    schema is now ordered and includes id

--- rest_api/schemas.py
+++ rest_api/schemas.py
@@ -2,10 +2,14 @@ from rest_api.models import User
 from marshmallow import Schema, fields, post_load
 
 class UserSchema(Schema):
+    class Meta:
+        ordered = True
+        
+    id = fields.Integer(default=None)
     name = fields.String()
     age = fields.Integer()
-    follower = fields.List(fields.Nested('UserSchema'), default=[])
-    following = fields.List(fields.Nested('UserSchema'), default=[])
+    follower = fields.List(fields.Nested('UserSchema', exclude=['follower', 'following']), default=[])
+    following = fields.List(fields.Nested('UserSchema', exclude=['follower', 'following']), default=[])
 
     @post_load
     def make_user(self, data, **kwargs):
